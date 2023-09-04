63046a472617ccabd56472d61b901a7361595a15
Tiago Pires <lucaspesteves2017@gmail.com>
2023-09-04 14:01:47 -0400

    modificando rotas de diretórios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 import '../css/style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import javascriptLogo from '../javascript.svg'
+import viteLogo from '../public/vite.svg'
 import { setupCounter } from '../counter.js'
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async ()=>{
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js',{type:"module"});
+      console.log('Service Worker registrada', reg);
+    } catch (err) {
+      console.log("service worker registro falhou:", err)
+      
+    }
+  })
+  
+}
+
+
+
+
+
+
+
 
 document.querySelector('#app').innerHTML = `
   <div>
