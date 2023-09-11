1afe4d619cf0e0fc5cba5a255613234e1189db0c
Tiago Pires <lucaspesteves2017@gmail.com>
2023-09-11 14:25:12 -0400

    adicionando botões

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-import  { openDB} from ' idb'
+import  { openDB} from 'idb'
 
 let db;
 
@@ -28,4 +28,24 @@ async function createDB() {
         showResult('erro ao criar o banco de dados'+ e.mensage)
         
     }   
+}
+
+window.addEventListener("DOMContentLoaded", async event =>{
+    createDB()
+    document.getElementById("input")
+    document.getElementById("btnSalvar").addEventListener("click",addData);
+    document.getElementById("btnListar").addEventListener("click",getData)
+})
+
+async function addData(){
+    const tx = await db .transaction('pessoas', 'readwrite');
+    const store = tx.objectStore('pessoas');
+    store.add({nome:'Lucas'});
+    await tx.done;
+
+}
+
+function showResult(text) {
+    document.querySelector('output').innerHTML = text;
+    
 }
