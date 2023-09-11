a3f54c5dc068cc6a5c931d287bb84c307ac8ef85
Tiago Pires <lucaspesteves2017@gmail.com>
2023-09-11 13:59:39 -0400

    criando o banco de dados na aplicação

--- js/db.js
+++ js/db.js
@@ -5,9 +5,27 @@ let db;
 async function createDB() {
 
     try {
+        db = await openDB('banco', 1,{
+            upgrade(db, oldVersion, newVersion, Transaction){
+                switch (oldVersion) {
+                    case 0:
+                    case 1:
+
+                    const store = db.createObjectStore('pessoas', {
+                        //A propiedade nome será o campo chave
+                        key: 'nome'
+                    })
+                    //Criando um indice id na store, deve star contido no objeto do bancod
+                      store.createIndex('id', 'id');
+                      showResult('Banco de dados foi criado!')
+
+                }
+            }
+        })
+        showResult('Banco de dados foi aberto!')
         
-    } catch () {
+    } catch (e) {
+        showResult('erro ao criar o banco de dados'+ e.mensage)
         
-    }
-    
+    }   
 }
